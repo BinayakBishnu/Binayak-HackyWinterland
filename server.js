006b2ebaac1873898002db0147a934ae3654bf4b
BinayakB <bishnu.binayak12@gmail.com>
2021-12-26 17:35:42 +0530

    feat: uploading to git repo

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
+
+
+const app = express()
+const u = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static(path.join(__dirname, 'html')))
+app.use(express.static(path.join(__dirname, 'styles')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.post('/signup', u, function (req, res) {
+    console.log('Signed In');
+
+    var response = {
+        name: req.body.signupmail,
+        password: req.body.signuppass
+    }
+
+    MongoClient.connect('mongodb://localhost:27017/', function (err, db) {
+        if (err)
+            throw err;
+        else
+            console.log("mongo connected!")
+        var dbo = db.db("studentdata");
+        dbo.collection('student').insertOne(response);
+    })
+    res.sendFile(path.join(__dirname, './pages/home.html'))
+})
+
+
+app.listen(3000, () => {
+    console.log('MLHSchools server running!')
+})
